ee159dcbff6696cc95e6baa63a6e775097df2d2e
mirkojp <mirkopavan88878@gmail.com>
2024-08-19 21:32:41 -0300

    Initial commit

---
+++ entrega19_8.js
@@ -0,0 +1,36 @@
+const apikey = "7F697BD5-D22A-4125-A343-0050A7A9DE67";
+
+async function fetchElements() {
+    const response = await fetch(`https://rest.coinapi.io/v1/exchanges`, {
+        headers: {
+            'X-CoinAPI-Key': apikey
+        }
+        });
+
+    const data = await response.json();
+
+    return data.slice(0, 16).map(item => ({
+        name: item.name,
+        volume_1hrs_usd: item.volume_1hrs_usd,
+        volume_1day_usd: item.volume_1day_usd
+    }));
+}
+
+// fetchElements().then(data => console.log(data)).catch(error => console.error(error));
+
+async function displayElements() {
+    const cryptoList = document.getElementById('crypto-list');
+    const cryptoData = await fetchElements();
+
+    if (cryptoData && cryptoData.length > 0){ //checks if truthy
+        cryptoData.forEach(element => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `Name: ${element.name}, Volume (1hr): $${element.volume_1hrs_usd}, Volume (1day): $${element.volume_1day_usd}`;
+            cryptoList.appendChild(listItem);
+        });
+    } else{
+        cryptoList.textContent = "No elements"
+    }   
+}
+
+displayElements()
